b12a09e4ab6a40b549a214633ee457c22abfdbe4
Brenda Sayuri <137084290+sayuribrenda@users.noreply.github.com>
2024-08-02 08:45:56 -0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let state = 0; // 0 = no movimento, 1 = movendo para a direita, 2 = movendo para a esquerda
+let state = 0; // 0 = centralizado, 1 = movendo para a direita, 2 = movendo para a esquerda
 const image = document.getElementById('fishImage');
 
 document.getElementById('moveButton').addEventListener('click', function() {
     if (state === 0) {
-        image.style.transform = 'translateX(100px)'; // Move a imagem para a direita
+        image.style.transform = 'translateX(calc(100% - 400px))'; // Move a imagem para a direita
         state = 1;
     } else if (state === 1) {
-        image.style.transform = 'translateX(-100px)'; // Move a imagem para a esquerda
+        image.style.transform = 'translateX(-calc(100% - 400px))'; // Move a imagem para a esquerda
         state = 2;
     } else if (state === 2) {
         image.style.transform = 'translateX(0)'; // Move a imagem de volta para o centro
